c9dd335b3e0feb2d62aa1b26505af590eadb7ea9
Frank Moda <frank@toasttab.com>
2018-10-31 15:19:59 -0400

    Resolving the tools dependency to unpack it must happen after the configuration phase

--- protoc-gen-kotlin/protoc-gen-kotlin-gradle-plugin/src/main/groovy/pbandk/gradle/plugin/KotlinProtobufPlugin.groovy
+++ protoc-gen-kotlin/protoc-gen-kotlin-gradle-plugin/src/main/groovy/pbandk/gradle/plugin/KotlinProtobufPlugin.groovy
@@ -2,6 +2,7 @@ package pbandk.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
 
 class KotlinProtobufPlugin implements Plugin<Project> {
     @Override
@@ -12,15 +13,15 @@ class KotlinProtobufPlugin implements Plugin<Project> {
 
         configureExtensions(project)
 
-        def pbandkVersion = determineVersion(project, ext)
+        def tools = configureTools(project)
+
+        def pbandkVersion = determineVersion(ext)
 
         if (ext.addRuntimeDependencies) {
             project.dependencies.add("compile", [group: "com.github.cretz.pbandk",
                                       name: "pbandk-runtime-jvm",
                                       version: pbandkVersion])
         }
-
-        def kotlinProtoExecutable = configureBinary(project, pbandkVersion)
 
         project.protobuf {
             generatedFilesBaseDir = "${project.buildDir}/generated-sources"
@@ -31,7 +32,7 @@ class KotlinProtobufPlugin implements Plugin<Project> {
             // This must be set to the batch file on Windows
             plugins {
                 kotlin {
-                    path = kotlinProtoExecutable
+                    path = getBinaryDirectory(getTargetDirectory(project, getToolsDirectory(project), pbandkVersion))
                 }
             }
 
@@ -44,13 +45,19 @@ class KotlinProtobufPlugin implements Plugin<Project> {
                     task.plugins {
                         kotlin {
                             option "kotlin_package=${ext.kotlinPackage}"
-                            option "kotlin_extra_classpath=${project.configurations.kotlinProtobufExtensions.asPath.replace(':', ';')}"
+                            option "kotlin_extra_classpath=${project.configurations.getByName("kotlinProtobufExtensions").asPath.replace(':', ';')}"
                             def kotlinSourceSetRoot = project.hasProperty("android") ? project.android : project
                             option "kotlin_extra_sourcepath=${kotlinSourceSetRoot.sourceSets.main.kotlin.srcDirs.join(';')}"
                         }
                     }
                 }
             }
+        }
+
+        project.afterEvaluate {
+
+            installBinary(project, pbandkVersion, tools)
+
         }
     }
 
@@ -59,7 +66,31 @@ class KotlinProtobufPlugin implements Plugin<Project> {
         project.configurations.getByName("compile").extendsFrom(extensionsConfiguration)
     }
 
-    def determineVersion(Project project, KotlinProtobufExtension extension) {
+    def configureTools(Project project) {
+        project.configurations.create("kotlinProtobufTools")
+        def tools = project.dependencies.add("kotlinProtobufTools", [
+                group: "com.github.cretz.pbandk",
+                name: "pbandk-protoc-gen-kotlin-jvm",
+                version: version,
+                classifier: "dist",
+                ext: "zip"
+        ])
+        return tools
+    }
+
+    def getToolsDirectory(Project project) {
+        return project.file("${project.rootProject.projectDir}/.gradle/tools")
+    }
+
+    def getTargetDirectory(Project project, File toolsDir, String version) {
+        return project.file("$toolsDir/protoc-gen-kotlin-$version")
+    }
+
+    def getBinaryDirectory(File targetDir) {
+        return "$targetDir/bin/protoc-gen-kotlin"
+    }
+
+    def determineVersion(KotlinProtobufExtension extension) {
         if (extension.toolsVersion) {
             return extension.toolsVersion
         } else {
@@ -72,29 +103,18 @@ class KotlinProtobufPlugin implements Plugin<Project> {
         }
     }
 
-    def configureBinary(Project project, String version) {
-        def toolsConfiguration = project.configurations.create("kotlinProtobufTools")
-        def tools = project.dependencies.add("kotlinProtobufTools", [
-                group: "com.github.cretz.pbandk",
-                name: "pbandk-protoc-gen-kotlin-jvm",
-                version: version,
-                classifier: "dist",
-                ext: "zip"
-        ])
-
-        def toolsDir = project.file("${project.rootProject.projectDir}/.gradle/tools")
+    def installBinary(Project project, String version, Dependency tools) {
+        def toolsDir = getToolsDirectory(project)
         toolsDir.mkdirs()
-        def targetDir = project.file("$toolsDir/protoc-gen-kotlin-$version")
+        def targetDir = getTargetDirectory(project, toolsDir, version)
 
         if (version.endsWith("-SNAPSHOT") || !targetDir.exists()) {
-            def toolsArchive = project.zipTree(toolsConfiguration.fileCollection(tools).singleFile)
+            def toolsArchive = project.zipTree(project.configurations.getByName("kotlinProtobufTools").fileCollection(tools).singleFile)
 
             project.copy {
                 from toolsArchive
                 into toolsDir
             }
         }
-
-        return "$targetDir/bin/protoc-gen-kotlin"
     }
 }
